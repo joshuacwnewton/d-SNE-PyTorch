5956f6558d2cce11d090c94349d01661247465ef
Joshua Newton <joshuacwnewton@gmail.com>
2020-04-08 20:44:29 -0700

    Add module docstring for metric.py

--- model/metric.py
+++ model/metric.py
@@ -1,4 +1,17 @@
+"""Functions for calculating evaluation metrics.
+
+As of 2020-04-08, PyTorch does not provide metrics in its core package.
+This file provides relevant metric calculations. However, other
+commonly used third-party packages include scikit-learn and Ignite.
+This is discussed further in an open PyTorch feature request, which
+suggests that metrics may become part of PyTorch Core in an upcoming
+release.
+
+    https://github.com/pytorch/pytorch/issues/22439
+"""
+
 import torch
+
 
 def accuracy(output, target):
     with torch.no_grad():
