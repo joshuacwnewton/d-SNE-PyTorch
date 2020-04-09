21777a141b05373759e1117a8d4dc43e0f0405fb
Joshua Newton <joshuacwnewton@gmail.com>
2020-04-09 16:09:58 -0700

    Move MetricTracker from utils.py to metrics.py

--- model/metrics.py
+++ model/metrics.py
@@ -11,6 +11,7 @@ release.
 """
 
 import torch
+import pandas as pd
 
 
 def accuracy(output, target):
@@ -30,3 +31,29 @@ def top_k_acc(output, target, k=3):
         for i in range(k):
             correct += torch.sum(pred[:, i] == target).item()
     return correct / len(target)
+
+
+class MetricTracker:
+    def __init__(self, *keys, writer=None):
+        self.writer = writer
+        self._data = pd.DataFrame(index=keys,
+                                  columns=['total', 'counts', 'average'])
+        self.reset()
+
+    def reset(self):
+        for col in self._data.columns:
+            self._data[col].values[:] = 0
+
+    def update(self, key, value, n=1):
+        if self.writer is not None:
+            self.writer.add_scalar(key, value)
+        self._data.total[key] += value * n
+        self._data.counts[key] += n
+        self._data.average[key] = self._data.total[key] / self._data.counts[
+            key]
+
+    def avg(self, key):
+        return self._data.average[key]
+
+    def result(self):
+        return dict(self._data.average)
--- pytorch_template/utils.py
+++ pytorch_template/utils.py
@@ -4,7 +4,6 @@ from itertools import repeat
 from collections import OrderedDict
 
 import numpy as np
-import pandas as pd
 import torch
 
 
@@ -37,27 +36,3 @@ def inf_loop(data_loader):
     ''' wrapper function for endless data loader. '''
     for loader in repeat(data_loader):
         yield from loader
-
-
-class MetricTracker:
-    def __init__(self, *keys, writer=None):
-        self.writer = writer
-        self._data = pd.DataFrame(index=keys, columns=['total', 'counts', 'average'])
-        self.reset()
-        
-    def reset(self):
-        for col in self._data.columns:
-            self._data[col].values[:] = 0
-
-    def update(self, key, value, n=1):
-        if self.writer is not None:
-            self.writer.add_scalar(key, value)
-        self._data.total[key] += value * n
-        self._data.counts[key] += n
-        self._data.average[key] = self._data.total[key] / self._data.counts[key]
-
-    def avg(self, key):
-        return self._data.average[key]
-    
-    def result(self):
-        return dict(self._data.average)
