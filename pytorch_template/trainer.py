5bbf19a759697529a74903cf34e534260e6d0a28
Joshua Newton <joshuacwnewton@gmail.com>
2020-04-10 12:02:31 -0700

    Fix incorrect call to metric tracker summary

--- pytorch_template/trainer.py
+++ pytorch_template/trainer.py
@@ -132,7 +132,7 @@ class DSNETrainer:
 
             # Print epoch summary to logger
             log = {'epoch': epoch}
-            log.update(self.metric_tracker.summary())
+            log.update(self.metric_tracker.summary)
             for key, value in log.items():
                 self.logger.info('    {:15s}: {}'.format(str(key), value))
 
