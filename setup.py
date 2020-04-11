c86daaaafc38a6e600715b754adec69ac5cc8358
Joshua Newton <joshuacwnewton@gmail.com>
2020-04-11 15:13:09 -0700

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+setup(
+    name='dsne_pytorch',
+    version='0.1.0',
+    packages=['dsne_pytorch'],
+    entry_points={
+        'console_scripts': [
+            'dsne_pytorch = dsne_pytorch.__main__:main'
+        ]
+    },
+    install_requires=[
+        "h5py",
+        "numpy",
+        "pandas",
+        "python-opencv",
+        "torch",
+        "torchvision",
+        "tensorboard"
+    ],
+    package_data={
+        # Include configuration files for loggers and experiments
+        "dsne_pytorch": ["configs/*.cfg", "configs/*.json"],
+    },
+
+    # PyPI metadata
+    author="Joshua Newton",
+    author_email="jnewt@uvic.ca",
+    description="A PyTorch port of the 'd-SNE' domain adaptation approach",
+    keywords="d-sne, dsne, domain adaptation, machine learning, deep learning",
+    project_urls={
+        "Source Code": "https://github.com/joshuacwnewton/d-SNE-PyTorch",
+    },
+)
